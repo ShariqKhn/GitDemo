a9b3c8f588e9d3850eb505516fdd65fb578bfd71
shariq <shariq1316@gmail.com>
2021-10-06 19:46:17 +0530

    this is second commit

--- src/TestHello.java
+++ src/TestHello.java
@@ -5,12 +5,14 @@ public class TestHello
 	public void race()
 	{
 		System.out.println("Increase Race");
+		System.out.println("Increase accelator");
 		
 	}
 	
 	public void breaks()
 	{
 		System.out.println("Take brake");
+		System.out.println("Take hand break");
 	}
 	
 	public static void main(String[] args) {
